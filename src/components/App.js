3860253ea07397814713176aac24482ce2871a52
ADITYA Raj <108565070+AdityaRajput732@users.noreply.github.com>
2023-02-05 13:06:29 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import '../styles/App.css';
+const App = () => {
+  
+
+  return (
+    <div id="main">
+
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+
+export default App;
